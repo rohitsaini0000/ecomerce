2e32a08e4d443d6a30bac8c5999721619f693f35
aadilzamindar <99026511+aadilzamindar@users.noreply.github.com>
2025-09-09 15:10:11 +0530

    hbasdfhbg

--- src/Component/Navber/Navbar.jsx
+++ src/Component/Navber/Navbar.jsx
@@ -19,7 +19,7 @@ function Navibar() {
       
       <div className="name">
         <ul>
-          <li><Link to="/">Home</Link></li>
+          <li><Link to="/">Homes</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/onsell">On Sell</Link></li>
           <li><Link to="/shop">Shop</Link></li>
